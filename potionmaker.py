ef63884c9b0187d3d35d66830f9be3f9a4e4f51c
klsegade <199424623+klsegade@users.noreply.github.com>
2026-02-15 02:16:59 -0800

    Added trait lists for plants/minerals/animals/fungi

--- potionmaker.py
+++ potionmaker.py
@@ -1,3 +1,5 @@
+import random
+
 '''Component Classes'''
 class Base:
     def __init__(self, name, modifiers, traits=None):
@@ -24,6 +26,14 @@ class Finisher:
         self.traits = traits or []
 
 
+'''Seed Input'''
+def traits_from_serial(serial, plant_type):
+    rng = random.Random(serial)  # deterministic seed
+
+    possible_traits = PLANT_TRAIT_POOLS[plant_type]
+
+    return rng.sample(possible_traits, k=2)
+
 '''Data Library'''
 components = {
     "water" : {
@@ -38,7 +48,7 @@ components = {
     },
     "alcohol" : {
         "category": "base",
-        "traits": [],
+        "traits": ['volatile', 'preservative'],
         "modifiers": {"stability":10, 'potency': 20}
     },
     "acid" : {
@@ -137,4 +147,42 @@ traits = {
     aqueous, oleaginous, volatile, acidic, alkaline, mineral, organic, metallic, carbonaceous, 
     combustive, stimulant, soothing, heating, cooling, drying, moistening, restorative, toxic, 
     luminous, shadowed, transmutative, fermentative, preservative, binding, catalytic
+}
+mineralTraits = {
+    "salts": ['preservative', 'absorbent', 'crystalline'],
+    "alkaline": ['alkaline'],
+    "oxidizers": ['volatile', 'flammable'],
+    "reactive metals": ['metallic', 'volatile', 'toxic'],
+    "pigments": ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'white', 'black', 'brown', 'staining', 'light-sensitive']
+}
+
+plantTraits = {
+    "medicinal": ['soothing', 'cleansing', 'protection', 'restorative', 'stimulant', 'binding'],
+    "nutritional": ['caloric', 'stimulant', 'fortifying', 'hydrating', 'restorative', 'acidic', 'alkaline', 'sugary', 'absorbent'],
+    "toxic": ['poisonous', 'paralytic', 'acidic', 'necrotic', 'hallucinogenic', 'volatile'],
+    "fermentable": ['sugary', 'starchy', 'acidic'],
+    "psychoactives": ['hallucinogenic', 'sedative', 'stimulant', 'euphoric', 'dissociative'],
+    "aromatics": ['aromatic', 'soothing', 'stimulant', 'preservative', 'repellant'],
+    "pigments": ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'white', 'black', 'brown', 'staining', 'light-sensitive'],
+    "industrial" : ['resinous', 'oily', 'fibrous', 'binding', 'flammable', 'alkaline', 'absorbent']
+}
+
+animalTraits = {
+    "lipids": ['binding', 'oily', 'waxy', 'protection', 'storing'],
+    "binders": ['binding', 'catalytic'],
+    "skeletal": ['alkaline', 'mineral'],
+    "venoms": ['paralytic', 'toxic']
+}
+
+fungalTraits = {
+    "fungi": ['fermentative', 'hallucinogenic', 'sedative', 'stimulant', 'euphoric', 'dissociative']
+}
+
+processedTraits = {
+    "distillates": ['volatile', 'concentrated', 'cleansing', 'acidic'],
+    "combustibles": ['alkaline', 'carbonaceous']
+}
+
+qualitiesOfCreation = {
+    Life, Elemental, Infusion, Mystic, Illusionary, Binding, Echo
 }
